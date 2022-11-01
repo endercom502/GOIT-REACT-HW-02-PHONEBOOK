9d2d50a4d25608f61dda8fff5264e9e38d903fa1
Andrzej Markiewicz <endercom502@gmail.com>
2022-11-01 23:37:14 +0100

    st

---
+++ src/components/ContactsForm/ContactForm.jsx
@@ -0,0 +1,11 @@
+import { nanoid } from "nanoid";
+import React from "react";
+
+import style from './ContactForm.module.css';
+
+const InitialState = {
+    id: '',
+    name: '',
+    number: '',
+};
+
